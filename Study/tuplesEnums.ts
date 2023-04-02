2ac53212cba8c4a2d850d2781ec4eb5e5c9fffff
nice130 <nice130@naver.com>
2023-04-02 23:04:17 +0900

    Enum

--- Study/tuplesEnums.ts
+++ Study/tuplesEnums.ts
@@ -27,3 +27,64 @@ goodRes.pop()
 
 const responses: HTTPResponse[] = [[404,"Not Fount"],[200,"OK"]];
 //위처럼 값을 넣을때에는 그 타입만 지켜주면 됨
+
+
+
+//Enum          --명명된 상수 집합을 정의할 수 있다...? 반복적으로 참조하는 값의 집합이 있을 경우에 사용
+//보통 0에서 1씩 증가하는 숫자를 사용하게 된다.
+//문자열로도 지정은 가능하다.
+//이 역시 자바스크립트에는 없음
+
+//위, 아래, 왼쪽, 오른쪽 // 대기중,발송됨, 발송완료  등
+
+enum OrderStatus { 
+    PENDING,    //0
+    SHIPPED,    //1
+    DELIVERED,  //2
+    RETURNED,   //3
+}
+const myStatus = OrderStatus.DELIVERED;
+//myStatus의 값은 2가 들어간다
+
+function isDelivered(status: OrderStatus){
+    return status === OrderStatus.DELIVERED;        //여기도 리턴2가된다.
+}
+
+
+enum  ArrowKeys {
+    UP = "up",
+    DOWN = "down",
+    LEFT = "left",
+    RIGHT = "right",
+    ERROR = 23,
+}
+//꼭 같은 타입이 들어갈 필요는 없다.
+
+ArrowKeys.UP //의 값은 "up"
+
+
+//enum은 실제로 JavaScript에 영향을 미치게 된다. 추가적인 코드로 나타내게 됨
+//위의 OrderStatus를 js로 바꾸면
+
+// var OrderStatus;
+// (function (OrderStatus) {
+//     OrderStatus[OrderStatus["PENDING"] = 0] = "PENDING";
+//     OrderStatus[OrderStatus["SHIPPED"] = 1] = "SHIPPED";
+//     OrderStatus[OrderStatus["DELIVERED"] = 2] = "DELIVERED";
+//     OrderStatus[OrderStatus["RETURNED"] = 3] = "RETURNED";
+// })(OrderStatus || (OrderStatus = {}));
+
+//위와 같이 나온다.
+
+const order ={
+    orderNumber: 32154,
+    status : OrderStatus.PENDING   
+}
+//위처럼 제시했을때 enum OrderStatus로 선언하면 똑같이 js에서 출력되지만
+//const enum OrderStatus로 선언을 하게되면 
+const order1 ={
+    orderNumber: 32154,
+    status : 0
+}
+
+//PENDING은 0의 값을 가지고 불변한 속성을 갖기 때문에 0으로 js에 표기된다. 문자열인경우에는 문자열로 바로 표기된다.
