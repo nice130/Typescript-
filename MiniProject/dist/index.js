cddd12fa67fadde177026b009ac45512126e6626
nice130 <nice130@naver.com>
2023-05-31 23:51:31 +0900

    dom다루기

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -4,3 +4,8 @@ function printDouble(msg) {
     console.log(msg);
 }
 printDouble("안녕!");
+//dom 다루기
+//document에 있는 모든것들은 타입이 정해져있다.
+//그래서 사용할때 타입을 지정해줘야한다.
+//"target": "es5" 이와같이 
+//다른버전에서 지원하지않는 target을쓸때에는 tsconfig.json에서 lib에 추가하여 사용하도록한다.
