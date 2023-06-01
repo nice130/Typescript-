c6eecdd79f653ad0cf5ef81fec1bdb51ffc70c20
nice130 <nice130@naver.com>
2023-06-01 23:11:10 +0900

    형변환해주기

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,15 +1,7 @@
-function printDouble(msg: string){
-    console.log(msg);
-    console.log(msg);
-}
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoinput")! as HTMLInputElement;    //형변환을 해줌
 
-printDouble("안녕!");
-
-
-
-
-//dom 다루기
-//document에 있는 모든것들은 타입이 정해져있다.
-//그래서 사용할때 타입을 지정해줘야한다.
-//"target": "es5" 이와같이 
-//다른버전에서 지원하지않는 target을쓸때에는 tsconfig.json에서 lib에 추가하여 사용하도록한다.
+btn.addEventListener("click",function(){
+    alert(input.value);
+    input.value='';
+})
